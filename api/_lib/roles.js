787c59a424cf99bd329c95b4a8d0cb101a15ee18
Mohavii <159702610+Mohavii@users.noreply.github.com>
2026-08-27 01:25:52 +0100

    Add supervision and CSCY permissions

--- api/_lib/roles.js
+++ api/_lib/roles.js
@@ -21,7 +21,7 @@
 const { sql } = require("./db");
 
 const DISPLAY_ROLES = ["president", "tresorier", "secretaire", "vpi", "vpe", "vpc", "supco_regional"];
-const CAPABILITIES = ["membership_approver", "report_validator", "pv_editor", "meeting_organizer", "project_manager"];
+const CAPABILITIES = ["membership_approver", "report_validator", "pv_editor", "meeting_organizer", "project_manager", "supervision_editor", "cscy_reviewer"];
 const NATIONAL_ROLES = ["president_national"];
 const MEMBERSHIP_STATUSES = ["nouveau_adherent", "adherent", "responsable", "senior", "membre_national", "ancien"];
 
